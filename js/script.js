54ee924c20812b0c7c0ffbed87b91723e89a80f8
dashulya11 <91964833+dashulya11@users.noreply.github.com>
2021-11-01 13:04:23 +0200

    init

--- js/script.js
+++ js/script.js
@@ -15,8 +15,10 @@ function modulo(num1, num2) {
     alert(result);
 }
 function mainFunction(callback) {
-    const num1 = +prompt('Введите первое число:');
-    const num2 = +prompt('Введите второе число:');
+    const num1 = prompt('Введите первое число:');
+    const num2 = prompt('Введите второе число:');
+    if (num1 === null || num1.trim() === `` || isNaN(+num1) || num2 === null || num2.trim() === `` || isNaN(+num2))
+        return alert(`some error`);
     callback(num1, num2);
 }
 
